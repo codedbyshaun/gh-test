96a2cd98864924ddd4f956ed7a287e3050f3b59c
Shaun Young <115547447+codedbyshaun@users.noreply.github.com>
2023-02-15 17:55:02 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 const ChatContainer = () => {
     return (
+        
         <div className="chat-container">
             <ChatHeader />
-
             <div>
                 <button className="option">Matches</button>
                 <button className="option">Chat</button>
             </div>
-
         <MatchesDisplay />
             <ChatDisplay />
         </div>
