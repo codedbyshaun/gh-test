5839924c2f0c24bf9d6c98f366246be077e3bcbc
Shaun Young <115547447+codedbyshaun@users.noreply.github.com>
2023-03-29 22:04:48 +0200

    Update app.js
    Add CardDeal feature

--- app.js
+++ app.js
@@ -66,4 +66,23 @@ const App = () => {
   </div>
 )
 
+import { card } from "../assets"
+import styles, { layout } from "../style"
+import Button from "./Button"
+
+const CardDeal = () =>  (
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2>Find a better card deal 
+          <br className="sm:block hidden" /> in a few easy steps.</h2>
+          <p></p>
+      </div>
+
+
+    </section>
+  )
+
+
+export default CardDeal
+
 export default ChatContainer
