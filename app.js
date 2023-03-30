a81401724996172965b368931b1dc2738288269b
Shaun Young <115547447+codedbyshaun@users.noreply.github.com>
2023-03-30 23:42:24 +0200

    Update app.js

--- app.js
+++ app.js
@@ -86,3 +86,23 @@ const CardDeal = () =>  (
 export default CardDeal
 
 export default ChatContainer
+
+import { feedback } from "../constants"
+import styles from "../style"
+import FeedbackCard from './FeedbackCard'
+
+const Testimonials = () => (
+  <section id="clients" className={`${styles.paddingY} 
+    ${styles.flexCenter} flex-col relative`}>
+    {/* TODO */}
+    <div className="absolute z-[10] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient " />
+    <div className="w-full flex justify-between items-center 
+      md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
+      <h1 className={styles.heading2}>What people are
+        <br className="sm:block hidden" /> saying about us</h1>
+      <div className="w-full md:mt-0 mt-6">
+        <p className={`${styles.paragraph} text-left mx-w-[450px]`}>Everything you need to accept card payments
+          and grow your business from anywhere on the planet.
+        </p>
+      </div>
+    </div>
