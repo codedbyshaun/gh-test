d30074a7273b95e73ae9df2da6db942cfbf40470
Shaun Young <115547447+codedbyshaun@users.noreply.github.com>
2023-05-17 22:03:43 +0200

    Update app.js
    Fix spacing

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ const App = () => {
         {authToken && <Route path="/onboarding" element={<OnBoarding />} />}
       </Routes>
     </BrowserRouter>
+         
   )
 }
          
@@ -55,6 +56,7 @@ const App = () => {
     <div className={`w-[64px] h-[64px] rounded-full ${styles.flexCenter} bg-dimBlue`}>
       <img src={icon} alt="icon" className="w-[50%] h-[50%] object-contain" />
     </div>
+    
     <div className="flex-1 flex flex-col ml-3 ">
       <h4 className="font-poppins font-semibold text-white text-[18px] leading-[23px] mb-1">
         {title}
